974268c057891e3abe0e39f1a9c3fc364bc6911c
franz <franzoni.stefano93@gmail.com>
2021-02-07 22:06:28 +0100

    Fix Footer

--- src/components/common/FooterComponent.tsx
+++ src/components/common/FooterComponent.tsx
@@ -7,10 +7,16 @@ const FooterComponent = (props: any) => {
     // <Menubar start={start} className="header"/>
     <div id="CustomStyle" className="background-primary">
       <div className="container-fluid footer">
-        <div className="row full-height justify-content-center align-items-buttom">
-          <div className="col-12 text-center bottom">
-            <div className="bottom">&copy; Copyright</div>
+        <div className="row full-height justify-content-center align-items-buttom bottom">
+          <div className="col-12 text-center">
+            <h2>
+              Made by{" "}
+              <a href="https://www.linkedin.com/in/stefano-franzoni-a7b512105/">
+                Stefano Franzoni
+              </a>
+            </h2>
           </div>
+          <div className="col-12 text-center">&copy; Copyright</div>
         </div>
       </div>
     </div>
